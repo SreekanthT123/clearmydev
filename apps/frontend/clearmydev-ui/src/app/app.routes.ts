b0771035127b0a77ec4872fbbd24da230e9b7447
SreekanthT123 <sreekanth@adysas.com>
2026-01-21 15:39:14 +0530

    feat: add ClearMyDev layout and navigation

--- apps/frontend/clearmydev-ui/src/app/app.routes.ts
+++ apps/frontend/clearmydev-ui/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout/layout.component';
+import { IncidentPage } from './layout/features/incidents/incident.page';
+import { JsonPage } from './layout/features/json/json.page';
+import { DiffsPage } from './layout/features/diffs/diffs.page';
+import { ErrorsPage } from './layout/features/errors/errors.page';
+import { LogsPage } from './layout/features/logs/logs.page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [],
+  },
+  { path: 'json', component: JsonPage },
+  { path: 'errors', component: ErrorsPage },
+  { path: 'logs', component: LogsPage },
+  { path: 'diffs', component: DiffsPage },
+  { path: 'incidents', component: IncidentPage },
+];
